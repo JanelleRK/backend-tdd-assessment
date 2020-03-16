4c62fbeab66c531c15be0762d5ca8f72d89bbbbb
Janelle Kuhns <j3ckuhns@gmail.com>
2020-03-16 16:45:41 -0700

    updated setUp in all def

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -8,40 +8,53 @@ import echo
 class TestEcho(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.parser = echo.create_parser()
+
 
     def test_upper_short(self):
         args = ["-u", "hello world"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
         actual = echo.main(args)
         expected = "HELLO WORLD"
         self.assertEqual(actual, expected)
 
     def test_lower_short(self):
         args = ['-l', "HeLlo WOrLd"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.lower)
         actual = echo.main(args)
         expected = "hello world"
         self.assertEqual(actual, expected)
 
     def test_title_short(self):
         args = ['-t', "HeLlo WOrLd"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.title)
         actual = echo.main(args)
         expected = "Hello World"
         self.assertEqual(actual, expected)
 
     def test_upper_long(self):
         args = ["--upper", "hello world"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
         actual = echo.main(args)
         expected = "HELLO WORLD"
         self.assertEqual(actual, expected)
 
     def test_lower_long(self):
         args = ['--lower', "HeLlo WOrLd"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.lower)
         actual = echo.main(args)
         expected = "hello world"
         self.assertEqual(actual, expected)
 
     def test_title_long(self):  
         args = ['--title', "HeLlo WOrLd"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.title)
         actual = echo.main(args)
         expected = "Hello World"
         self.assertEqual(actual, expected)
