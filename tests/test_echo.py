7c23644087a1d0842e45be3a6e6c4b6e02a27e75
Janelle Kuhns <j3ckuhns@gmail.com>
2020-03-16 17:26:02 -0700

    finished all

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -3,13 +3,50 @@
 
 import unittest
 import echo
+import subprocess
+import sys
 
 # Your test case class goes here
 class TestEcho(unittest.TestCase):
 
     def setUp(self):
         self.parser = echo.create_parser()
+        self.pystring = 'Python2'
+        if sys.version_info[0] == 3:
+            self.pystring = 'Python3'
 
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
+
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect it's output.
+        process = subprocess.Popen(
+            [self.pystring, "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        with open("./USAGE") as f:
+            usage = f.read()
+        self.assertEquals(stdout, usage)
+
+    def no_options(self):
+        args = ["HeLlo WOrLd"]
+        ns = self.parser.parse_args(args)
+        self.assertFalse(ns.upper)
+        self.assertFalse(ns.lower)
+        self.assertFalse(ns.title)
+        actual = echo.main(args)
+        expected = args[0]
+        self.assertEqual(actual, expected)
+
+    def test_all_options(self):
+        args = ["-tul", "HeLlo WOrLd"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
+        self.assertTrue(ns.lower)
+        self.assertTrue(ns.title)
+        actual = echo.main(args)
+        expected = "Hello World"
+        self.assertEqual(actual, expected)
 
     def test_upper_short(self):
         args = ["-u", "hello world"]
